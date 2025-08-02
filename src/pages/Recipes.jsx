70287a919a2b6003eff1743faedb0bc7a28a3868
Josiah <jowebdev5021@gmail.com>
2025-08-02 10:12:26 -0500

    update styles

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 export default function Recipes() {
+
+
+  useEffect(() => {
+
+
+
+  }, [])
+
   return (
-    <div>Recipes</div>
+    <div className='recipes-container'>
+      <div className='my-recipes'>
+        <h2>My Recipes</h2>
+        <div className='recipes-container'>
+            <button>+ <br></br>Add new recipe</button>
+        </div>
+      </div>
+      <div className='community-recipes'>
+         <h2>Community Recipes</h2>
+        <div className='recipes-container'>
+        
+        </div>
+      </div>
+    </div>
   )
 }
