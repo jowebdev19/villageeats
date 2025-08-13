fe79aec0fa05e5fd7f8e635e5317cdfebc0ffe6c
Josiah <jowebdev5021@gmail.com>
2025-08-13 16:22:57 -0500

    api server working

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+
+const port = 3000
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/meal', (req, res) => {
+
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then((response) => response.json())
+    .then((meal) => {
+        console.log(meal)
+        return res.send(meal)
+    })
+});
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Server running on port ${port}`)
+})
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import { useEffect } from 'react'
 
@@ -6,21 +7,41 @@ export default function Recipes() {
 
   useEffect(() => {
 
+    async function getMeal(){
+      try{
+         await fetch('http://localhost:3000/meal',
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          console.log(data.meals)
+        })
+      } catch (err){
+        throw new Error()
+      }
+    }
 
 
+    getMeal()
   }, [])
 
   return (
     <div className='recipes-container'>
       <div className='my-recipes'>
         <h2>My Recipes</h2>
-        <div className='recipes-container'>
+        <div className='card-container'>
             <button>+ <br></br>Add new recipe</button>
         </div>
       </div>
       <div className='community-recipes'>
          <h2>Community Recipes</h2>
-        <div className='recipes-container'>
+        <div className='card-container'>
         
         </div>
       </div>
